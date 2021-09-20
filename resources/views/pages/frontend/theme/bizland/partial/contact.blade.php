66a4054fb99fa821be214f90d42333e276b0aac4
Naufal Haidir Ridha <naufalhaidirridha@rocketmail.com>
2021-09-20 12:17:27 +0700

    Fix Maps

--- resources/views/pages/frontend/theme/bizland/partial/contact.blade.php
+++ resources/views/pages/frontend/theme/bizland/partial/contact.blade.php
@@ -38,7 +38,7 @@
 
       <div class="col-lg-6 ">
         <!-- <iframe class="mb-4 mb-lg-0" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12097.433213460943!2d-74.0062269!3d40.7101282!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xb89d1fe6bc499443!2sDowntown+Conference+Center!5e0!3m2!1smk!2sbg!4v1539943755621" frameborder="0" style="border:0; width: 100%; height: 384px;" allowfullscreen></iframe> -->
-        <iframe class="mb-4 mb-lg-0" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4710.300575615101!2d107.57305386697838!3d-6.905563495976081!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e68e675924fe861%3A0x659d23fb6e1c66a1!2sJl.%20Maleber%20Utara%2C%20Kec.%20Andir%2C%20Kota%20Bandung%2C%20Jawa%20Barat%2040184!5e0!3m2!1sen!2sid!4v1632114899343!5m2!1sen!2sid" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+        <iframe class="mb-4 mb-lg-0" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4710.300575615101!2d107.57305386697838!3d-6.905563495976081!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e68e675924fe861%3A0x659d23fb6e1c66a1!2sJl.%20Maleber%20Utara%2C%20Kec.%20Andir%2C%20Kota%20Bandung%2C%20Jawa%20Barat%2040184!5e0!3m2!1sen!2sid!4v1632114899343!5m2!1sen!2sid" frameborder="0" style="border:0; width: 100%; height: 384px;" allowfullscreen></iframe>
         MAPS DISABLED
       </div>
 
